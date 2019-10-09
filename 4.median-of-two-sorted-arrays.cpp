2c257be2bd08da141929b6f98fecedd02c26fdaa
XZeusJ <394386241@qq.com>
2019-10-09 09:48:14 +0800

    update files

--- 4.median-of-two-sorted-arrays.cpp
+++ 4.median-of-two-sorted-arrays.cpp
@@ -6,27 +6,45 @@
 
 // @lc code=start
 class Solution {
-public:
+   public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int len1 = nums1.size();
-        int len2 = nums2.size();
-        
-        int start = 0;
-        int end = len1;
-        int partitionX = (int) ((start + end) / 2);
-        int partitionY = (int) ((len1 + len2 + 1) / 2 - partitionX); // invariable point partitionX + partitionY = (x+y+1)/2
-        
-        while ((nums1[partitionX-1] <= nums2[partitionY] && nums2[partitionY-1] <= nums1[partitionX]) != True) {
-            if (nums1[partitionX-1] > nums2[partitionY])
-                // move towards left in X
-            else
-                // move towards right in X
+        // if nums1 length is greater than switch them so that nums1 is smaller than nums2
+        if (nums1.size() > nums2.size()) {
+            return findMedianSortedArrays(nums2, nums1);
         }
-            
-        
-            
-        
+
+        int x = nums1.size();
+        int y = nums2.size();
+
+        int low  = 0;
+        int high = x;
+
+        while (low <= high) {
+            int partitionX = (low + high) / 2;
+            int partitionY = (x + y + 1) / 2 - partitionX;  // invariable point partitionX + partitionY = (x+y+1)/2
+
+            // edge case: if partitionX is 0, it means nothing is there on left side. Use -INF for maxLeftX
+            // same for right side
+            int maxLeftX  = (partitionX == 0) ? INT_MIN : nums1[partitionX - 1];
+            int minRightX = (partitionX == 0) ? INT_MAX : nums1[partitionX];
+
+            int maxLeftY  = (partitionY == 0) ? INT_MIN : nums1[partitionY - 1];
+            int minRightY = (partitionY == 0) ? INT_MAX : nums1[partitionY];
+
+            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+                // we have partitioned array at correct place
+                // now compute median
+                if ((x + y) % 2 == 0) {
+                    return (max(maxLeftX, maxLeftY) + min(minRightX, minRightY)) / 2;
+                } else {
+                    return max(maxLeftX, maxLeftY);
+                }
+            } else if (maxLeftX > minRightY) {
+                high = partitionX - 1;
+            } else {
+                low = partitionX + 1;
+            }
+        }
     }
 };
 // @lc code=end
-

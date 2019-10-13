92aa09027b3fd1dfc8c1006e1f485a98cc2d0620
XZeusJ <394386241@qq.com>
2019-10-13 15:05:17 +0800

    update files

---
+++ 31.next-permutation.cpp
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode id=31 lang=cpp
+ *
+ * [31] Next Permutation
+ *
+ * https://leetcode.com/problems/next-permutation/description/
+ *
+ * algorithms
+ * Medium (31.14%)
+ * Likes:    2247
+ * Dislikes: 727
+ * Total Accepted:    278.8K
+ * Total Submissions: 894.6K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * Implement next permutation, which rearranges numbers into the
+ * lexicographically next greater permutation of numbers.
+ * 
+ * If such arrangement is not possible, it must rearrange it as the lowest
+ * possible order (ie, sorted in ascending order).
+ * 
+ * The replacement must be in-place and use only constant extra memory.
+ * 
+ * Here are some examples. Inputs are in the left-hand column and its
+ * corresponding outputs are in the right-hand column.
+ * 
+ * 1,2,3 → 1,3,2
+ * 3,2,1 → 1,2,3
+ * 1,1,5 → 1,5,1
+ * 
+ */
+
+// @lc code=start
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        
+    }
+};
+// @lc code=end
+

7470ee9c38071675cb2f4fb3f6e438f2221ffb74
XZeusJ <394386241@qq.com>
2019-10-23 08:10:23 +0800

    update files

---
+++ 345.reverse-vowels-of-a-string.cpp
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode id=345 lang=cpp
+ *
+ * [345] Reverse Vowels of a String
+ *
+ * https://leetcode.com/problems/reverse-vowels-of-a-string/description/
+ *
+ * algorithms
+ * Easy (42.27%)
+ * Likes:    457
+ * Dislikes: 847
+ * Total Accepted:    177.6K
+ * Total Submissions: 419.2K
+ * Testcase Example:  '"hello"'
+ *
+ * Write a function that takes a string as input and reverse only the vowels of
+ * a string.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "hello"
+ * Output: "holle"
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "leetcode"
+ * Output: "leotcede"
+ * 
+ * 
+ * Note:
+ * The vowels does not include the letter "y".
+ * 
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string reverseVowels(string s) {
+        int i = 0, j = s.size() - 1;
+        while (i < j) {
+            i = s.find_first_of("aeiouAEIOU", i);
+            j = s.find_last_of("aeiouAEIOU", j);
+            if (i < j) {
+                swap(s[i++], s[j--]);
+            }
+        }
+        return s;
+    }
+};
+// @lc code=end
+

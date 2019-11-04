b423dc614daf08699f2a3972955bb2306216e9d9
XZeusJ <394386241@qq.com>
2019-11-04 13:50:11 +0800

    update files

---
+++ 101.symmetric-tree.cpp
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode id=101 lang=cpp
+ *
+ * [101] Symmetric Tree
+ *
+ * https://leetcode.com/problems/symmetric-tree/description/
+ *
+ * algorithms
+ * Easy (44.57%)
+ * Likes:    2805
+ * Dislikes: 62
+ * Total Accepted:    488.1K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[1,2,2,3,4,4,3]'
+ *
+ * Given a binary tree, check whether it is a mirror of itself (ie, symmetric
+ * around its center).
+ * 
+ * For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+ * 
+ * 
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   2
+ * ⁠/ \ / \
+ * 3  4 4  3
+ * 
+ * 
+ * 
+ * 
+ * But the following [1,2,2,null,3,null,3] is not:
+ * 
+ * 
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   2
+ * ⁠  \   \
+ * ⁠  3    3
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * Bonus points if you could solve it both recursively and iteratively.
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        return !root || is(root->left, root->right);
+    }
+    
+    bool is(TreeNode* p, TreeNode* q) {
+        if (!p && !q) return true;
+        if (!p || !q) return false;
+        return p->val == q->val && is(p->left, q->right) && is(p->right, q->left);
+    }
+};
+// @lc code=end
+
--- 102.binary-tree-level-order-traversal.cpp
+++ 102.binary-tree-level-order-traversal.cpp
@@ -50,10 +50,30 @@
  * };
  */
 class Solution {
-public:
+   public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        
+        vector<vector<int>> result;
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            int n = q.size();
+            vector<int> crtLevel;
+            crtLevel.reserve(n);
+            for (int i = 0; i < n; i++) {
+                TreeNode* p = q.front();
+                q.pop();
+                if (p != nullptr) {
+                    crtLevel.push_back(p->val);
+                    q.push(p->left);
+                    q.push(p->right);
+                }
+            }
+
+            if (crtLevel.size() > 0)
+                result.push_back(move(crtLevel));
+        }
+
+        return result;
     }
 };
 // @lc code=end
-

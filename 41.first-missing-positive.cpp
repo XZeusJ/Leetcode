0145f2c8f743b511eb80a98d1c71612cf5f83281
XZeusJ <394386241@qq.com>
2019-11-15 20:17:08 +0800

    update files

--- 41.first-missing-positive.cpp
+++ 41.first-missing-positive.cpp
@@ -48,19 +48,18 @@
 using namespace std;
 
 class Solution {
-public:
+   public:
     int firstMissingPositive(vector<int>& nums) {
-        std::unordered_set<int> s(nums.begin(), nums.end());
-        
-        int minPos = 1;
-        while(true) {
-            if (s.count(minPos)) {
-                minPos++;
-            } else {
-                return minPos;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                swap(nums[i], nums[nums[i] - 1]);
             }
         }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) return i + 1;
+        }
+        return n + 1;
     }
 };
 // @lc code=end
-

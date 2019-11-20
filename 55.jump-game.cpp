db61419507c94235bd90e2bbbea7ca094c2facf8
XZeusJ <394386241@qq.com>
2019-11-20 20:12:18 +0800

    update files

--- 55.jump-game.cpp
+++ 55.jump-game.cpp
@@ -44,10 +44,14 @@
 
 // @lc code=start
 class Solution {
-public:
+   public:
     bool canJump(vector<int>& nums) {
-        
+        vector<int> dp(nums.size(), 0);
+        for (int i = 1; i < nums.size(); i++) {
+            dp[i] = max(dp[i - 1], nums[i - 1]) - 1;
+            if (dp[i] < 0) return false;
+        }
+        return true;
     }
 };
 // @lc code=end
-

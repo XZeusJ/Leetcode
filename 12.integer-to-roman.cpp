f7a0ed03bc0e079222ebcfc24ff475c2943ffe28
XZeusJ <394386241@qq.com>
2019-12-09 18:23:41 +0800

    update files

---
+++ 12.integer-to-roman.cpp
@@ -0,0 +1,107 @@
+/*
+ * @lc app=leetcode id=12 lang=cpp
+ *
+ * [12] Integer to Roman
+ *
+ * https://leetcode.com/problems/integer-to-roman/description/
+ *
+ * algorithms
+ * Medium (52.29%)
+ * Likes:    759
+ * Dislikes: 2261
+ * Total Accepted:    287.5K
+ * Total Submissions: 544K
+ * Testcase Example:  '3'
+ *
+ * Roman numerals are represented by seven different symbols: I, V, X, L, C, D
+ * and M.
+ * 
+ * 
+ * Symbol       Value
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+ * 
+ * For example, two is written as II in Roman numeral, just two one's added
+ * together. Twelve is written as, XII, which is simply X + II. The number
+ * twenty seven is written as XXVII, which is XX + V + II.
+ * 
+ * Roman numerals are usually written largest to smallest from left to right.
+ * However, the numeral for four is not IIII. Instead, the number four is
+ * written as IV. Because the one is before the five we subtract it making
+ * four. The same principle applies to the number nine, which is written as IX.
+ * There are six instances where subtraction is used:
+ * 
+ * 
+ * I can be placed before V (5) and X (10) to make 4 and 9. 
+ * X can be placed before L (50) and C (100) to make 40 and 90. 
+ * C can be placed before D (500) and M (1000) to make 400 and 900.
+ * 
+ * 
+ * Given an integer, convert it to a roman numeral. Input is guaranteed to be
+ * within the range from 1 to 3999.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 3
+ * Output: "III"
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 4
+ * Output: "IV"
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: 9
+ * Output: "IX"
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: 58
+ * Output: "LVIII"
+ * Explanation: L = 50, V = 5, III = 3.
+ * 
+ * 
+ * Example 5:
+ * 
+ * 
+ * Input: 1994
+ * Output: "MCMXCIV"
+ * Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+ * 
+ */
+
+// @lc code=start
+class Solution {
+   public:
+    string intToRoman(int num) {
+        string res = "";
+        vector<char> roman{'M', 'D', 'C', 'L', 'X', 'V', 'I'};
+        vector<int> value{1000, 500, 100, 50, 10, 5, 1};
+        for (int n = 0; n < 7; n += 2) {
+            int x = num / value[n];
+            if (x < 4) {
+                for (int i = 1; i <= x; i++) res += roman[n];
+            } else if (x == 4) {
+                res = res + roman[n] + roman[n - 1];
+            } else if (x > 4 && x < 9) {
+                res += roman[n - 1];
+                for (int i = 6; i <= x; i++) res += roman[n];
+            } else if (x == 9) {
+                res = res + roman[n] + roman[n - 2];
+            }
+            num %= value[n];
+        }
+        return res;
+    }
+};
+// @lc code=end

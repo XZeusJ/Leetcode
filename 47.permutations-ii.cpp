72b203e4d26c5282b3e8725025ea95ee2224bf78
XZeusJ <394386241@qq.com>
2020-01-06 22:24:08 +0800

    update files

---
+++ 47.permutations-ii.cpp
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=47 lang=cpp
+ *
+ * [47] Permutations II
+ *
+ * https://leetcode.com/problems/permutations-ii/description/
+ *
+ * algorithms
+ * Medium (42.45%)
+ * Likes:    1474
+ * Dislikes: 51
+ * Total Accepted:    299.1K
+ * Total Submissions: 686.4K
+ * Testcase Example:  '[1,1,2]'
+ *
+ * Given a collection of numbers that might contain duplicates, return all
+ * possible unique permutations.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,1,2]
+ * Output:
+ * [
+ * ⁠ [1,1,2],
+ * ⁠ [1,2,1],
+ * ⁠ [2,1,1]
+ * ]
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+   public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        
+    }
+};
+// @lc code=end

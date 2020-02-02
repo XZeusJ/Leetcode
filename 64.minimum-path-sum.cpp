f0791f9074dd6e556526758a8d7e73f7fdb6d3e3
XZeusJ <394386241@qq.com>
2020-02-02 22:55:56 +0800

    update files

--- 64.minimum-path-sum.cpp
+++ 64.minimum-path-sum.cpp
@@ -39,11 +39,19 @@ class Solution {
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
         int dp[m][n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (i == m - 1 && j != n - 1)
+                    dp[i][j] = grid[i][j] + dp[i][j + 1];
+                else if (i != m - 1 && j == n - 1)
+                    dp[i][j] = grid[i][j] + dp[i + 1][j];
+                else if (i != m - 1 && j != n - 1)
+                    dp[i][j] = grid[i][j] + min(dp[i + 1][j], dp[i][j + 1]);
+                else
+                    dp[i][j] = grid[i][j];
             }
         }
+        return dp[0][0];
     }
 };
 // @lc code=end

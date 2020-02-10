c96dc5bdca2717e010ea45a06c7860df13b0fac4
XZeusJ <394386241@qq.com>
2020-02-10 22:34:01 +0800

    update files

--- 92.reverse-linked-list-ii.cpp
+++ 92.reverse-linked-list-ii.cpp
@@ -37,19 +37,31 @@
  */
 class Solution {
    public:
-    ListNode *reverseBetween(ListNode *head, int m, int n) {
-        ListNode *dummy = new ListNode(-1), *pre = dummy;
-        dummy->next = head;
+    ListNode *successor = NULL;
 
-        for (int i = 0; i < m - 1; i++) pre = pre->next;  // get first required point
-        ListNode *cur = pre->next;
-        for (int i = m; i < n; i++) {
-            ListNode *t = cur->next;  // break down t and former relation
-            cur->next   = t->next;    // breakdown 2 and 3, realte 2 to 4
-            t->next     = pre->next;  // put 3 after 1
-            pre->next   = t;          // breakdown 1 and 2, relate 1 to 3
+    ListNode *reverseN(ListNode *head, int n) {
+        if (n == 1) {
+            // 记录第 n+1 个节点
+            successor = head->next;
+            return head;
         }
-        return dummy->next;
+        // 以 head->next 为起点, 需要反转前 n - 1 个节点
+        ListNode *last = reverseN(head->next, n - 1);
+
+        head->next->next = head;
+        // 让反转之后的 head 节点和后面的节点连起来
+        head->next = successor;
+        return last;
+    }
+
+    ListNode *reverseBetween(ListNode *head, int m, int n) {
+        // base case
+        if (m == 1) {
+            return reverseN(head, n);
+        }
+        // 前进到反转的起点触发 base case
+        head->next = reverseBetween(head->next, m - 1, n - 1);
+        return head;
     }
 };
 // @lc code=end
---
+++ 93.restore-ip-addresses.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=93 lang=cpp
+ *
+ * [93] Restore IP Addresses
+ *
+ * https://leetcode.com/problems/restore-ip-addresses/description/
+ *
+ * algorithms
+ * Medium (32.57%)
+ * Likes:    951
+ * Dislikes: 418
+ * Total Accepted:    167.7K
+ * Total Submissions: 500.3K
+ * Testcase Example:  '"25525511135"'
+ *
+ * Given a string containing only digits, restore it by returning all possible
+ * valid IP address combinations.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: "25525511135"
+ * Output: ["255.255.11.135", "255.255.111.35"]
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<string> restoreIpAddresses(string s) {
+        
+    }
+};
+// @lc code=end
+

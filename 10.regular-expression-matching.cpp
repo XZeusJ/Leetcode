9e95a1dcf09b7663c6637a9a33c79513891f6e03
XZeusJ <394386241@qq.com>
2020-02-12 21:29:50 +0800

    update files

--- 10.regular-expression-matching.cpp
+++ 10.regular-expression-matching.cpp
@@ -89,7 +89,7 @@ class Solution {
    public:
     bool isMatch(string s, string p) {
         if (p.empty()) return s.empty();
-        bool first_match = (!s.empty()) && (s[0] == p[0] || s[0] == '.');
+        bool first_match = (!s.empty()) && (s[0] == p[0] || p[0] == '.');
 
         if (p.size() >= 2 && p[1] == '*') {
             return isMatch(s, p.substr(2)) || (first_match && isMatch(s.substr(1), p));

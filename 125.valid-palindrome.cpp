32960d3a289c60c33c26af1a0ca8bb28e921b377
XZeusJ <394386241@qq.com>
2020-03-21 22:53:21 +0800

    update files

---
+++ 125.valid-palindrome.cpp
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode id=125 lang=cpp
+ *
+ * [125] Valid Palindrome
+ *
+ * https://leetcode.com/problems/valid-palindrome/description/
+ *
+ * algorithms
+ * Easy (32.53%)
+ * Likes:    942
+ * Dislikes: 2577
+ * Total Accepted:    504.5K
+ * Total Submissions: 1.5M
+ * Testcase Example:  '"A man, a plan, a canal: Panama"'
+ *
+ * Given a string, determine if it is a palindrome, considering only
+ * alphanumeric characters and ignoring cases.
+ * 
+ * Note: For the purpose of this problem, we define empty string as valid
+ * palindrome.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "A man, a plan, a canal: Panama"
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "race a car"
+ * Output: false
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+   public:
+    bool isPalindrome(string s) {
+        if (s.size() <= 1) return true;
+        int i = 0, j = s.size() - 1;
+        while (i < j) {
+            while (i < j && !isalnum(s[i])) i++;
+            while (i < j && !isalnum(s[j])) j--;
+            if (tolower(s[i++]) != tolower(s[j--]))
+                return false;
+        }
+        return true;
+    }
+};
+// @lc code=end

6e1999f63a55c97541b4d13c3d680283ed375c4e
XZeusJ <394386241@qq.com>
2020-05-28 18:13:32 +0800

    update files

--- 1438.longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1438.longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -1,7 +1,8 @@
 /*
  * @lc app=leetcode id=1438 lang=cpp
  *
- * [1438] Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit
+ * [1438] Longest Continuous Subarray With Absolute Diff Less Than or Equal to
+ * Limit
  *
  * https://leetcode.com/problems/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/description/
  *
@@ -16,18 +17,18 @@
  * Given an array of integers nums and an integer limit, return the size of the
  * longest continuous subarray such that the absolute difference between any
  * two elements is less than or equal to limit.
- * 
+ *
  * In case there is no subarray satisfying the given condition return 0.
- * 
- * 
+ *
+ *
  * Example 1:
- * 
- * 
+ *
+ *
  * Input: nums = [8,2,4,7], limit = 4
- * Output: 2 
- * Explanation: All subarrays are: 
+ * Output: 2
+ * Explanation: All subarrays are:
  * [8] with maximum absolute diff |8-8| = 0 <= 4.
- * [8,2] with maximum absolute diff |8-2| = 6 > 4. 
+ * [8,2] with maximum absolute diff |8-2| = 6 > 4.
  * [8,2,4] with maximum absolute diff |8-2| = 6 > 4.
  * [8,2,4,7] with maximum absolute diff |8-2| = 6 > 4.
  * [2] with maximum absolute diff |2-2| = 0 <= 4.
@@ -35,43 +36,55 @@
  * [2,4,7] with maximum absolute diff |2-7| = 5 > 4.
  * [4] with maximum absolute diff |4-4| = 0 <= 4.
  * [4,7] with maximum absolute diff |4-7| = 3 <= 4.
- * [7] with maximum absolute diff |7-7| = 0 <= 4. 
+ * [7] with maximum absolute diff |7-7| = 0 <= 4.
  * Therefore, the size of the longest subarray is 2.
- * 
- * 
+ *
+ *
  * Example 2:
- * 
- * 
+ *
+ *
  * Input: nums = [10,1,2,4,7,2], limit = 5
- * Output: 4 
+ * Output: 4
  * Explanation: The subarray [2,4,7,2] is the longest since the maximum
  * absolute diff is |2-7| = 5 <= 5.
- * 
- * 
+ *
+ *
  * Example 3:
- * 
- * 
+ *
+ *
  * Input: nums = [4,2,2,2,4,4,2,2], limit = 0
  * Output: 3
- * 
- * 
- * 
+ *
+ *
+ *
  * Constraints:
- * 
- * 
+ *
+ *
  * 1 <= nums.length <= 10^5
  * 1 <= nums[i] <= 10^9
  * 0 <= limit <= 10^9
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
 class Solution {
-public:
+   public:
     int longestSubarray(vector<int>& nums, int limit) {
-        
+        multiset<int> s;
+
+        int res = 0;
+        int l = 0;
+
+        for (int r = 0; r < nums.size(); r++) {
+            s.insert(nums[r]);
+            while (*s.rbegin() - *s.begin() > limit) {
+                s.erase(s.find(nums[l]));
+                l++;
+            }
+            res = max(res, r - l + 1);
+        }
+        return res;
     }
 };
 // @lc code=end
-
